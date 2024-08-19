82e2ac29befc257eff72f4667f2c7b8294375909
DouglasGomes32 <douglasgabriel.rdp@hotmail.com>
2024-08-19 18:11:08 -0300

    Adicionando Remover dos Favoritos

--- magalu_favoritos/clientes/views.py
+++ magalu_favoritos/clientes/views.py
@@ -1,12 +1,33 @@
-from rest_framework import viewsets # type: ignore
+from rest_framework import viewsets
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 from .models import Cliente, ProdutoFavorito
-from .serializers import ClienteSerializer, ProdutoFavoritoSerializer # type: ignore
+from .serializers import ClienteSerializer, ProdutoFavoritoSerializer
 
 
 class ClienteViewSet(viewsets.ModelViewSet):
     queryset = Cliente.objects.all()
     serializer_class = ClienteSerializer
 
+
 class ProdutoFavoritoViewSet(viewsets.ModelViewSet):
     queryset = ProdutoFavorito.objects.all()
     serializer_class = ProdutoFavoritoSerializer
+    def get_queryset(self):
+        cliente_id = self.request.query_params.get('cliente_id')
+        if cliente_id:
+            return ProdutoFavorito.objects.filter(cliente_id=cliente_id)
+        else:
+            return ProdutoFavorito.objects.none()
+
+
+class RemoverProdutoFavorito(APIView):
+    def delete(self, request, cliente_id, produto_id):
+        try:
+            # Tenta encontrar o produto favorito para o cliente específico
+            produto_favorito = ProdutoFavorito.objects.get(cliente_id=cliente_id, produto_id=produto_id)
+            produto_favorito.delete()  # Remove o produto favorito da lista
+            return Response(status=status.HTTP_204_NO_CONTENT)  # Retorna sucesso com conteúdo vazio
+        except ProdutoFavorito.DoesNotExist:
+            return Response({"error": "Produto não encontrado na lista de favoritos"}, status=status.HTTP_404_NOT_FOUND)
--- magalu_favoritos/magalu_favoritos/urls.py
+++ magalu_favoritos/magalu_favoritos/urls.py
@@ -1,12 +1,8 @@
-from django.contrib import admin
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter # type: ignore
-from clientes.views import ClienteViewSet, ProdutoFavoritoViewSet
-from django.urls import path
-from magalu_favoritos.views import ( # type: ignore
-    ClienteListCreateView,
-    ClienteRetrieveUpdateDeleteView,
-    ProdutoFavoritoListCreateView,
+from rest_framework.routers import DefaultRouter
+from clientes.views import (
+    ClienteViewSet,
+    ProdutoFavoritoViewSet,
     RemoverProdutoFavorito
 )
 
@@ -15,8 +11,7 @@ router.register(r'clientes', ClienteViewSet)
 router.register(r'produtos_favoritos', ProdutoFavoritoViewSet)
 
 urlpatterns = [
-    path('clientes/', ClienteListCreateView.as_view(), name='clientes'),
-    path('clientes/<int:pk>/', ClienteRetrieveUpdateDeleteView.as_view(), name='cliente-detail'),
-    path('produtos_favoritos/', ProdutoFavoritoListCreateView.as_view(), name='produtos_favoritos'),
+    path('api/', include(router.urls)),
+    path('api/', include(router.urls)), 
     path('produtos_favoritos/<int:cliente_id>/<int:produto_id>/', RemoverProdutoFavorito.as_view(), name='remover_produto_favorito'),
 ]
